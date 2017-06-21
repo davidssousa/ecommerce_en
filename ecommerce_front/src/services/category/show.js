f698456f5b183ca969fd36fde5b446772645a56b
David Sousa <david.sousa@enbrasil.local>
2017-06-21 19:28:26 -0300

    design

--- ecommerce_front/src/services/category/show.js
+++ ecommerce_front/src/services/category/show.js
@@ -1,7 +1,32 @@
+const Product = require('./../../schemas/product')
+const Category = require('./../../schemas/category')
+
 module.exports = (req, res) => {
-	return res.render('category/show', {
-		title: 'Category',
-		layout: 'layouts/main',
-		user: req.user || undefined	
-	})
+	Category
+		.find({
+			slug: req.params.slug
+		})
+		.then((category) => {
+			Product
+				.find({
+					enable: true,
+					category
+				})
+				.then((products) => {
+
+					return res.render('category/show', {
+						title: 'Category',
+						layout: 'layouts/main',
+						user: req.user || undefined,
+						products	
+					})
+				})
+				.catch((error) => {
+
+				})
+		})
+		.catch((error) => {
+
+		})
+	
 }
